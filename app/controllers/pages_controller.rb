9ee75f93a9e4dcc53455f856888e950c9a7af7de
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-10-31 02:43:29 +0000

    added pages_controller and outcome html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,36 @@
 class PagesController < ApplicationController
+
+  Options = ["rock", "paper", "scissors"]
+  Outcomes = {"rock"=> {"rock"=> "tie", "paper"=> "win", "scissors"=> "lose"},
+            "paper"=> {"rock"=> "lose", "paper"=> "tie", "scissors"=> "win"},
+            "scissors"=> {"rock"=> "win", "paper"=> "lose", "scissors"=> "tie"}}
+
   def home
     render({:template => "pages_templates/home"})
+  end
+
+  def process_opponent_and_outcome(our_choice)
+    opponent = Options.sample
+    outcome = Outcomes[opponent][our_choice]
+    return opponent, outcome
+  end
+
+  def rock
+    @our_choice = "rock"
+    @opponent_choice, @outcome = process_opponent_and_outcome(@our_choice)
+    render({:template => "pages_templates/outcome"})
+  end
+
+  def paper
+    @our_choice = "paper"
+    @opponent_choice, @outcome = process_opponent_and_outcome(@our_choice)
+    render({:template => "pages_templates/outcome"})
+  end
+
+  def scissors
+    @our_choice = "scissors"
+    @opponent_choice, @outcome = process_opponent_and_outcome(@our_choice)
+    puts @our_choice, @opponent_choice
+    render({:template => "pages_templates/outcome"})
   end
 end
